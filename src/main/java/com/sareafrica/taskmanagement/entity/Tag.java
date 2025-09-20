89c796271c2d6054f832aac8a888e1c0481573e1
kelvin joseph <kelvinnjoseph77@gmail.com>
2025-09-20 23:29:23 +0300

    feat: add Task, Tag, and Status entities with relationships

---
+++ src/main/java/com/sareafrica/taskmanagement/entity/Tag.java
@@ -0,0 +1,28 @@
+package com.sareafrica.taskmanagement.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "tags")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Tag {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @ManyToMany(mappedBy = "tags")
+    private Set<Task> tasks = new HashSet<>();
+}
